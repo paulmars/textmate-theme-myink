958ebb07e34c7d225c357b4b0b382c91c2f26aab
Paul McKellar <paul.mckellar@gmail.com>
2010-01-23 14:06:59 -0800

    my ink install:

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/Library/Application\ Support/TextMate/Themes
-cp MyInk.tmTheme ~/Library/Application\ Support/TextMate/Themes
+# rm ~/Library/Application\ Support/TextMate/Themes/MyInk.tmTheme
+ln -s MyInk.tmTheme ~/Library/Application\ Support/TextMate/Themes/MyInk.tmTheme
